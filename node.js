64774245cedb9577003dae770a2e417f7b0b520b
Prudent NGAIPERA-DONG <111011332+Prudent777@users.noreply.github.com>
2023-03-11 13:38:26 +0100

    node.js 2

--- node.js
+++ node.js
@@ -2,3 +2,23 @@ const myButton = document.getElementById('my-button');
 myButton.addEventListener('click', () => {
   alert('You clicked the button!');
 });
+const dropdown = document.querySelector('.dropdown');
+const dropdownContent = dropdown.querySelector('.dropdown-content');
+
+dropdown.addEventListener('click', () => {
+  dropdownContent.classList.toggle('show');
+});
+
+dropdownContent.addEventListener('click', (event) => {
+  if (event.target.tagName === 'A') {
+    window.location.href = event.target.href;
+  }
+});
+
+document.addEventListener('click', (event) => {
+  if (!dropdown.contains(event.target)) {
+    dropdownContent.classList.remove('show');
+  }
+});
+const productImages = document.querySelector('.product-images');
+const mainImage = productImages
